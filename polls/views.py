dc0244c0ade43264ffe4b20367a9d48f0d7abd05
Kozlov Nikita <kozlovzxc@gmail.com>
2021-11-16 23:10:03 +0300

    feat(polls): added basic templates

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hey, it\'s a polls app')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question not found')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})

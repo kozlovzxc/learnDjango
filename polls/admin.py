a47c2df927693211c3acad85ce72e43ec2194e5a
Kozlov Nikita <kozlovzxc@gmail.com>
2021-11-17 18:23:06 +0300

    feat(common): editing admin template

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
+
 from .models import Question, Choice
 
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    fields = ['choice_text']
+    model = Choice
+    extra = 2
+
+
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['question_text', 'pub_date']
+    fieldsets = (
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    )
+    inlines = [ChoiceInline]
+
+    list_display = ['question_text', 'pub_date']
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
